b58bb883156b22d8c7bbd0a0c72e86c32c8ae676
Hanss <hanss.rivera@gmail.com>
2024-08-25 07:13:50 -0600

    flask routes proper setup

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,35 @@
+import requests
 from flask import Flask
 from AppClasses import IGDB
 from flask_cors import CORS
 IGDB_INSTANCE = IGDB()
 
-
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-def home():
+def startup():
     IGDB_INSTANCE.init_token()
-    IGDB_INSTANCE.quick_search("doom eternal")
+
+@app.route("/quick_search")
+def quick_search():
+    query =  requests.args.get("query")
+    IGDB_INSTANCE.quick_search(query)
     return IGDB_INSTANCE.data
+
+
+
+
+
+
+
+
+@app.route("/dev")
+def dev():
+    IGDB_INSTANCE.init_token()
+    IGDB_INSTANCE.quick_search("final fantasy")
+    return IGDB_INSTANCE.data
+
 
     
 
