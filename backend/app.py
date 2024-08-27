9343f9ff34990415b4e2525c779603aa9c8c2dfb
Hanss <hanss.rivera@gmail.com>
2024-08-27 14:42:56 -0600

    better app.py logic and format

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,10 @@ import requests
 from flask import Flask
 from AppClasses import IGDB
 from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 IGDB_INSTANCE = IGDB()
-
-
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-    app.run(debug=True)
-    IGDB_INSTANCE.init_token()
 
 @app.route("/quick_search")
 def quick_search():
@@ -17,22 +13,12 @@ def quick_search():
     IGDB_INSTANCE.quick_search(query)
     return IGDB_INSTANCE.data
 
-
-
-
-
-
-
-
 @app.route("/dev")
 def dev():
     IGDB_INSTANCE.init_token()
     IGDB_INSTANCE.quick_search("final fantasy")
     return IGDB_INSTANCE.data
 
-
-    
-
-
 if __name__ == "__main__":
-    create_app()
+    app.run(debug=True)
+    IGDB_INSTANCE.init_token()
