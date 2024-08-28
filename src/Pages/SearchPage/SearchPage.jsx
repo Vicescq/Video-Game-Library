a7abb96b20574d1bd62da278a0b64e14d0329b5f
Hanss <hanss.rivera@gmail.com>
2024-08-28 07:23:15 -0600

    site colour changes, boilerplate code on SearchPage

---
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -0,0 +1,12 @@
+import SearchPageS from "./SearchPage.module.css"
+
+function SearchPage(){
+
+    return(
+        <>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nulla facere, inventore consequatur exercitationem, recusandae dolores eius, eligendi praesentium nisi natus ducimus iusto eaque quo? Nostrum dolore alias aspernatur incidunt!
+        </div>
+        </>
+    )
+}
